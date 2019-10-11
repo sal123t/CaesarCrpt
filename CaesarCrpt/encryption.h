749891ef24d8faa7b579904128cc98fbf10fd4b2
Ivan <56439107+sal123t@users.noreply.github.com>
2019-10-11 23:06:15 +0300

    prj
    Some dbg config added

--- CaesarCrpt/encryption.h
+++ CaesarCrpt/encryption.h
@@ -3,6 +3,17 @@
 #ifndef ENCRYPTION_H
 #define ENCRYPTION_H
 
+/* Debug memory allocation support */
+#ifdef _DEBUG
+# define _CRTDBG_MAP_ALLOC
+# include <crtdbg.h>
+# define SetDbgMemHooks() \
+_CrtSetDbgFlag(_CRTDBG_LEAK_CHECK_DF | _CRTDBG_CHECK_ALWAYS_DF | \
+_CRTDBG_ALLOC_MEM_DF | _CrtSetDbgFlag(_CRTDBG_REPORT_FLAG))
+#else /* _DEBUG */
+# define SetDbgMemHooks() ((VOID)0)
+#endif /* _DEBUG */
+
 int crpt_txt(char* FilePath, int Key);
 
 #endif /* ENCRYPTION_H */
